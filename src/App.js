37b2410c070d70c47249c77f3324c36d22c2b7ad
Juri <juriparladore@gmail.com>
2021-09-08 15:00:04 +0200

    adding header compionent and link to home in the logo and contacts(not yet working)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import HomePage from './components/pages/homePage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './components/pages/shopPage/shop-page.component';
-
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
        <Route exact path='/' component={HomePage }/>
       <Route  path='/shop' component={ShopPage }/> 
